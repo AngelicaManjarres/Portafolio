8f1d2bfa5b17073656f410e0bddfd19cbf905610
Angelica <angelica_maria91@hotmail.com>
2018-03-01 14:36:17 -0500

    Diseño de la cabecera

--- gulp/tareas/estilos.js
+++ gulp/tareas/estilos.js
@@ -1,14 +1,14 @@
 var gulp = require('gulp');
 var postcss = require('gulp-postcss');
-var variables = require('postcss-simple-vars');
 var prefixer = require('autoprefixer');
 var cssImport = require('postcss-import');
 var mixins = require('postcss-mixins');
 var nested = require('postcss-nested');
+var variables = require('postcss-simple-vars');
 
 gulp.task('estilos', function(){
 	return gulp.src('./app/css/**/*.css')
-	.pipe(postcss([prefixer, cssImport, variables, mixins, nested]))
+	.pipe(postcss([prefixer, cssImport, mixins, nested, variables]))
 	.on('error', function(errorInfo){
 		console.log(errorInfo.toString());
 		this.emit('end');
